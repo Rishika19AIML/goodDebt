8e264b6eaa1b89e0e987735521a887d0c6cfb325
Rishika Sharma <rishikas.official@gmail.com>
2025-08-29 14:35:14 +0530

    database.py

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/loan_db"
 engine = create_engine(DATABASE_URL)

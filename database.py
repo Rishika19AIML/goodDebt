02088f87f9ee90f9d59a389e0cda451bc0a3fdfe
Rishika Sharma <rishikas.official@gmail.com>
2025-08-30 13:30:45 +0530

    database.py

--- database.py
+++ database.py
@@ -1,9 +1,19 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/loan_db"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# ✅ yeh function add karo
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
